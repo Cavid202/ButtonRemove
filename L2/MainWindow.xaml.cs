3f44393b567b365980e51ad1fc1b86a7ac0dcd04
Bayr_lv09 <Bayr_lv09@STHQ0126-16>
2022-06-05 09:39:41 +0400

    Add project files.

---
+++ L2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L2
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button buttonn )
+            {
+                Random rand = new Random();
+                buttonn.Background = new SolidColorBrush(Color.FromRgb((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255)));
+                MessageBox.Show($"{buttonn.Name}");
+
+
+            }
+        }
+
+        private void button1_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button buttonn)
+            {
+
+                wrppnl.Children.Remove(buttonn);
+               
+            }
+        }
+    }
+}
